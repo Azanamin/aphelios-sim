a19889c504c22384e32742eb5270d9ab241f5849
Azanamin <minhnhat912002@gmail.com>
2022-10-26 20:21:56 +0700

    new commit

--- src/View/components/GunInfoSubPanel.js
+++ src/View/components/GunInfoSubPanel.js
@@ -1,18 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 
 function GunInfoSubPanel( props ) {
 
-    return (
+    const [gun, setGun] = useState(undefined);
+
+    useEffect(() => {
+        setGun(props.gun)
+        console.log(gun);
+    }, [props.gun])
+
+    return gun ? (
         <>
-            <h5 style={{color: props.gun.description.hexCode}}>
+            <h5 style={{color: gun.description.hexCode}}>
                 <img style={{width: "30px", height: "30px", marginRight: "10px"}}
-                    src={"assets/mini_icon/" + props.gun.name + ".png"}
+                    src={"assets/mini_icon/" + gun.name + ".png"}
                     alt="" >
                 </img>
-                {props.gun.name}
+                {gun.name}
             </h5>
         </>
-    );
+    ) : <></>;
 }
 
 export default GunInfoSubPanel;
